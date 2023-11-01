0b8d47ed4f7e612d9f93fa6181d2818a9b6cdaec
gylman <gylmanbm@gmail.com>
2023-11-01 18:48:07 +0900

    feat/implement-purple-section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './index.css';
 function App() {
   return (
     <>
-      <TopAlert />;
+      <TopAlert />
       <WhiteSection />
     </>
   );
--- src/components/WhiteSection.tsx
+++ src/components/WhiteSection.tsx
@@ -56,6 +56,35 @@ const Arrow = styled.img`
   margin-left: 260px;
 `;
 
+const PurpleWrapper = styled.div`
+  display: flex;
+  background: #291346;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 208px 171px 0px 171px;
+`;
+
+const TrueToDec = styled.p`
+  color: #ff7a00;
+  font-family: Atyp Display;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%; /* 41.6px */
+  margin-bottom: 34px;
+`;
+
+const BuiltIn = styled.div`
+color: #FFF;
+text-align: center;
+font-family: Atyp Display;
+font-size: 80px;
+font-style: normal;
+font-weight: 400;
+line-height: 120%; /* 96px *
+`;
+
 const WhiteSection = () => {
   return (
     <Container>
@@ -71,6 +100,13 @@ const WhiteSection = () => {
         </Eco>
         <Arrow src={arrow} alt='arrow_right' />
       </EcoWrapper>
+      <PurpleWrapper>
+        <TrueToDec>TRUE TO DECENTRALIZATION</TrueToDec>
+        <BuiltIn>
+          Built-in defensibility and trustless capabilities with{' '}
+          <strong>zero knowledge</strong>
+        </BuiltIn>
+      </PurpleWrapper>
     </Container>
   );
 };

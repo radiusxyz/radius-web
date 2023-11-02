ba2e5d5c45d4654d03cfe5d9737eed3b8063f62f
gylman <gylmanbm@gmail.com>
2023-11-02 09:14:52 +0900

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import BssCards from './components/BSSCards';
+import BssCards from './components/BssCards';
 import TopAlert from './components/TopAlert';
 import WhiteSection from './components/WhiteSection';
 import './index.css';

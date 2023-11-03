e9677c6f9ec078f6665f0facd98d9293431a47c2
gylman <gylmanbm@gmail.com>
2023-11-03 10:12:18 +0900

    implement research areas

--- src/components/ResearchAreas.tsx
+++ src/components/ResearchAreas.tsx
@@ -1,25 +1,19 @@
 import styled from 'styled-components';
+import x from '../assets/images/x.svg';
 const Container = styled.div`
   height: 100%;
   width: 100%;
   background: #ffaf37;
+  display: flex;
+  justify-content: center;
 `;
 const Content = styled.div`
   max-width: 1400px;
   width: 100%;
-`;
-const Tags = styled.div`
+  padding: 237px 45px 161px 45px;
   display: flex;
-  gap: 20px;
-`;
-const Tag = styled.div`
-  display: flex;
-  padding: 21px 46px;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  gap: 10px;
-  border-radius: 66px;
-  background: rgba(255, 255, 255, 0.3);
 `;
 
 const Text = styled.p`
@@ -30,6 +24,42 @@ const Text = styled.p`
   font-style: normal;
   font-weight: 400;
   line-height: 130%; /* 54.6px */
+  margin-bottom: 58px;
+`;
+
+const Tags = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 159px;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+const Tag = styled.div`
+  display: flex;
+  padding: 21px 46px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 66px;
+  background: rgba(255, 255, 255, 0.3);
+  color: #fff;
+  text-align: center;
+  font-family: Gilroy-SemiBold;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%; /* 31.2px */
+`;
+
+const SubText = styled.div`
+  color: rgba(0, 0, 0, 0.9);
+  text-align: center;
+  font-family: Atyp Display;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%; /* 31.2px */
+  margin-top: 32px;
 `;
 
 const tagTexts = [
@@ -45,16 +75,20 @@ const ResearchAreas = () => {
   return (
     <Container>
       <Content>
-        <Tags>
-          {tagTexts.map((tag) => (
-            <Tag key={tag}>{tag}</Tag>
-          ))}
-        </Tags>
         <Text>
           Our research areas span across a wide range of topics including
           designs, methodologies, and technologies — the building blocks that
           contribute to the advancement of rollups
         </Text>
+        <Tags>
+          {tagTexts.map((tag) => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </Tags>
+        <img src={x} width='63px' alt='x' />
+        <SubText>
+          Interested in collaboration? We'd love to hear from you!
+        </SubText>
       </Content>
     </Container>
   );

b16222e9839ec38c3f062e58bf4cd8d73d888968
gylman <gylmanbm@gmail.com>
2023-11-05 00:50:42 +0900

    implement cards section

--- src/components/EcoPage.tsx
+++ src/components/EcoPage.tsx
@@ -1,5 +1,82 @@
 import styled from 'styled-components';
 import logo from '../assets/images/logo.svg';
+import entanglement from '../assets/images/entanglement.svg';
+
+const IntroSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const IntroBackground = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const IntroContent = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  position: absolute;
+  top: calc(50%-20px);
+`;
+
+const IntroText = styled.div`
+  max-width: 697px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const IntroMiniTitle = styled.p`
+  color: #ff7a00;
+  font-family: Atyp Display;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%; /* 28.8px */
+  margin-bottom: 51px;
+`;
+
+const IntroTitle = styled.p`
+  color: #000;
+  font-family: Atyp Display;
+  font-size: 82px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%; /* 98.4px */
+  max-width: 568px;
+  margin-bottom: 37px;
+`;
+
+const IntroDetails = styled.p`
+  color: #585858;
+  font-family: Gilroy-Regular;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%; /* 31.2px */
+`;
+
+const CardsSection = styled.div`
+  width: 100%;
+  background: #1b191f;
+  padding: 250px 0 16px;
+  display: flex;
+  justify-content: center;
+`;
+
+const CardsSectionContent = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1400px;
+`;
+
+const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 19px 18px;
+`;
 
 const Card = styled.div`
   border-radius: 12px;
@@ -10,7 +87,7 @@ const Card = styled.div`
   padding: 32px;
   display: flex;
   flex-direction: column;
-  gap: 59px;
+  gap: 60px;
   min-height: 276px;
 `;
 
@@ -32,7 +109,7 @@ const Logo = styled.div`
   justify-content: center;
 `;
 
-const Title = styled.span`
+const CardTitle = styled.span`
   color: #fff;
   font-family: Gilroy-Bold;
   font-size: 24px;
@@ -41,7 +118,7 @@ const Title = styled.span`
   line-height: 130%; /* 31.2px */
 `;
 
-const Details = styled.p`
+const CardDetails = styled.p`
   color: #fff;
   font-family: Gilroy-Regular;
   font-size: 16px;
@@ -50,18 +127,109 @@ const Details = styled.p`
   line-height: 140.625%; /* 22.5px */
 `;
 
+const cards = [
+  {
+    logo: logo,
+    title: 'ALTLAYER',
+    details:
+      'AltLayer is an open and decentralized protocol for developers to launch highly elastic application-tailored rollups.',
+  },
+  {
+    logo: logo,
+    title: 'caldera',
+    details:
+      'Caldera specializes in building high-performance, customizable, and application-specific layer-two blockchains.',
+  },
+  {
+    logo: logo,
+    title: 'CELESTIA',
+    details:
+      'Celestia is a modular data availability network that securely scales with the number of users, making it easy for anyone to launch their own blockchain.',
+  },
+  {
+    logo: logo,
+    title: 'HERODOTUS',
+    details:
+      'Herodotus is a powerful data access middleware that provides smart contracts with synchronous access to both current and historical on-chain data across Ethereum layers.',
+  },
+  {
+    logo: logo,
+    title: 'LUMOZ',
+    details:
+      'Lumoz (Previously Opside) is a decentralized ZK-RaaS platform as well as a PoW network that facilitates ZKP mining.',
+  },
+  {
+    logo: logo,
+    title: 'MADARA',
+    details:
+      'Madara is standard, customizable and blazing fast Starknet sequencer. Madara delivers unmatched performance and scalability to power Starknet-based Validity Rollup chain.',
+  },
+  {
+    logo: logo,
+    title: 'Parama',
+    details:
+      'Pragma is a decentralized, transparent and composable oracle network, leveraging state-of-the-art zero-knowledge cryptography.',
+  },
+  {
+    logo: logo,
+    title: 'Rollkit',
+    details:
+      'Rollkit is a rollup framework that gives developers the freedom to deploy rollups throughout the modular stack.',
+  },
+  {
+    logo: logo,
+    title: 'SAGA',
+    details:
+      'Saga is a protocol for automatically provisioning application-specific blockchains.',
+  },
+  {
+    logo: logo,
+    title: 'Stacker',
+    details:
+      'Stackr is a modular SDK for building micro-rollups in general-purpose languages.',
+  },
+  {
+    logo: logo,
+    title: 'Vistara',
+    details:
+      'Vistara is a one-click rollup deployment framework for designing and managing scalable infrastructure.',
+  },
+];
+
 const EcoPage = () => {
   return (
-    <Card>
-      <LogoTitle>
-        <Logo />
-        <Title>ALTLAYER</Title>
-      </LogoTitle>
-      <Details>
-        AltLayer is an open and decentralized protocol for developers to launch
-        highly elastic application-tailored rollups.
-      </Details>
-    </Card>
+    <>
+      <IntroSection>
+        <IntroBackground src={entanglement} />
+        <IntroContent>
+          <IntroText>
+            <IntroMiniTitle>OUR ECOSYSTEM</IntroMiniTitle>
+            <IntroTitle>Meet builders and partners</IntroTitle>
+            <IntroDetails>
+              We're all about making rollups more decentralized, sustainable,
+              and scalable. We collaborate with like-minded builders and
+              infrastructure partners to drive our research and development
+              efforts, working together to shape a decentralized future.
+            </IntroDetails>
+          </IntroText>
+        </IntroContent>
+      </IntroSection>
+      <CardsSection>
+        <CardsSectionContent>
+          <Cards>
+            {cards.map((card) => (
+              <Card>
+                <LogoTitle>
+                  <Logo />
+                  <CardTitle>{card.title}</CardTitle>
+                </LogoTitle>
+                <CardDetails>{card.details}</CardDetails>
+              </Card>
+            ))}
+          </Cards>
+        </CardsSectionContent>
+      </CardsSection>
+    </>
   );
 };
 
--- src/components/WhiteSection.tsx
+++ src/components/WhiteSection.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import lab from '../assets/images/lab.svg';
-import radius from '../assets/images/radius.svg';
 import arrow from '../assets/images/arrow.svg';
 import purple_card_1 from '../assets/images/purple_card_1.svg';
 import purple_card_2 from '../assets/images/purple_card_2.svg';
@@ -37,86 +36,6 @@ const Content = styled.div`
   max-width: 1400px;
   position: absolute;
   height: 100%;
-`;
-
-const TopRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-top: 19px;
-`;
-
-const EcoDocGit = styled.div`
-  display: flex;
-  border-radius: 34px;
-  max-width: 600px;
-  width: 100%;
-  height: 50px;
-  background: #fff;
-  justify-content: center;
-  align-items: center;
-`;
-
-const EcoBtn = styled.button`
-  color: #000;
-  font-family: Gilroy-SemiBold;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 130%; /* 23.4px */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  background: transparent;
-  width: 100%;
-  padding: 14px 35px 13px 91px;
-  border-radius: 34px 0px 0px 34px;
-  &:hover {
-    cursor: pointer;
-    color: #fff;
-    background: #ff5c02;
-  }
-`;
-const DocBtn = styled.button`
-  color: #000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: Gilroy-SemiBold;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 130%; /* 23.4px */
-  border: none;
-  background: transparent;
-  padding: 14px 35px 13px 35px;
-  &:hover {
-    cursor: pointer;
-    color: #fff;
-    background: #ff5c02;
-  }
-`;
-const GitBtn = styled.button`
-  color: #000;
-  font-family: Gilroy-SemiBold;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 130%; /* 23.4px */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  width: 100%;
-  background: transparent;
-  padding: 14px 90px 13px 35px;
-  border-radius: 0px 34px 34px 0px;
-  &:hover {
-    cursor: pointer;
-    color: #fff;
-    background: #ff5c02;
-  }
 `;
 
 const IntroWrapper = styled.div`
@@ -228,10 +147,6 @@ const BuiltIn = styled.div`
   font-weight: 400;
   line-height: 120%;
   margin-bottom: 111px;
-`;
-
-const StyledLink = styled.a`
-  text-decoration: none;
 `;
 
 type Card = {
